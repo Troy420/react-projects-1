ee751bfc8d7d9f0bde7a1ca65e0dd2a48f2df8e8
Bernard <bernardy_ho@hotmail.com>
2021-12-20 15:43:59 +0700

    slider tutorial project

---
+++ 07-slider/setup/src/Title.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <section className="section">
+      <div className="title">
+        <h3>
+          <span>/</span>reviews
+        </h3>
+      </div>
+    </section>
+  );
+};
+
+export default Title;
